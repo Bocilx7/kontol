a249ae162e5a723d04402223e4e78433042192f7
Bocilx7 <53088482+Bocilx7@users.noreply.github.com>
2019-07-21 15:35:16 +0700

    Add files via upload

---
+++ bengkelFacebook.sh
@@ -0,0 +1,307 @@
+#!/bin/bash
+clear
+HT='\033[0;32m' #Hijau tua
+HM='\033[32;1m' #Hijau muda#
+Bt='\033[0;36m' #Biru tua
+Bm='\033[36;1m' #Biru muda
+M='\033[31;1m' #Merah
+P='\033[37;1m' #Putih
+H='\033[30;1m' #Hitam
+O='\033[33;1m' #Orange
+K='\033[1;33m' #Kuning
+C='\033[36;1m' #cyan
+
+
+figlet -f slant BengkelFb | lolcat
+figlet -f slant Mr.Bocilx7 | lolcat
+
+
+echo "##########################################" |lolcat
+echo "
+             ______________
+              ║▒▒▒▒▒▒▒▒▒▒║
+              ║▒▒▒▒▒▒▒▒▒▒║
+              ║▒▒▒▒▒▒▒▒▒▒║
+             ╔════════════╗
+             ╚════════════╝             # Tools bengkelFB
+              ║██████████╚╗
+              ║██╔══╗█╔═╗█║             # Author : Mr.Bocilx7
+              ║██║╬╔╝█╚╗║█║             # Team   : ERROR CYBER TEAM
+              ║██╚═╝█║█╚╝█║             # WA     : 085747407420
+              ╚╗█████████═╝             # InstaG : Mr.Bocilx7
+               ╚╗║╠╩╩╩╩╩╝               #info    : jngn recode anjing
+                ║║┈┈┈█▐█████▒.｡oO       # Dont Recode Please Ya Asw
+                ║██╠╦╦╦╗
+                ╚╗██████
+                 ╚════╝"|lolcat
+
+
+
+echo
+echo
+sleep 2
+echo $Bm "[•]====================================================[•]"
+echo $K "[+]Author    : Mr.Bocilx7                       [+]"
+echo $P "[+]Whasapp   : 085747407420                           [+]"
+echo $M "[+]Note      : gunakan dengan bijak                    [+]"
+echo $C "[+]facebook  : Mr.Bocilx7                    [+]"
+echo $M "[•]===================================================[•]"
+echo
+sleep 1.5
+echo $C"================="$P"===================="$HT"["$C"A"$HT"]"
+echo $K"✓         [1]"$H"BRUTEFORCE FB         ✓"$HT"["$C"G"$HT"]"
+echo $K"✓         [2]"$H"REPORT FB             ✓"$HT"["$C"U"$HT"]"
+echo $K"✓         [3]"$H"DARK FB 1.6           ✓"$HT"["$C"U"$HT"]"
+echo $K"✓         [4]"$H"YAHOO CLONING         ✓"$HT"["$C"N"$HT"]"
+echo $K"✓         [5]"$H"MBF (Massal)          ✓"$HT"["$C"G"$HT"]"
+echo $K"✓         [6]"$HM"Dark FB 1.7           ✓"$HT"["$C"B"$HT"]"
+echo $K"✓         [7]"$O"TOKEN FB              ✓"$HT"["$C"O"$HT"]"
+echo $K"✓         [8]"$O"FBH                   ✓"$HT"["$C"C"$HT"]"
+echo $K"✓         [9]"$HM"Dark FB (noCheckpoint)✓"$HT"["$C"R"$HT"]"
+echo $K"✓         [10]"$HM"BotFb                ✓"$HT"["$C"R"$HT"]"
+echo $k"✓         [11]"$HM"FbGuard              ✓"$HT"["$C"T"$HT"]"
+echo $k"✓         [12]"$HM"TokenFACEBOOK        ✓"$HT"["$C"E"$HT"]"
+echo $k"✓         [13]"$HM"facebot              ✓"$HT"["$C"A"$HT"]"
+echo $k"✓         [14]"$HM"mbfBot               ✓"$HT"["$C"M"$HT"]"
+echo $k"✓         [15]"$HM"autoCrackAkunCp(gratis) ✓"$HT"["$C"C"$HT"]"
+echo $k"✓         [16]"$HM"HACK FACEBOOK MBF    ✓"$HT"["$C"C"HT"]"
+echo $k"✓         [17]"$HM"drak1.7 ✓"$HT"["$C"C"K"]"
+echo $C"================="$P"===================="$HT"["$C"Y"$HT"]"
+echo   "                                        ✓"$HT"["$C"B"$HT"]"
+sleep 1
+echo $bt">>>>>>>>>>>>>>>>>>"$P"<<<<<<<<<<<<<<<<<<"$HT"["$C"R"$HT"]"
+echo $bt">         [99]"$HM"Install              <"$HT"["$C"R"$HT"]"
+echo $bt">         [00]"$HM"Keluar              <"$HT"["$C"R"$HT"]"
+echo $bt">>>>>>>>>>>>>>>>>>"$P"<<<<<<<<<<<<<<<<<<"$HT"["$C"R"$HT"]"
+
+read -p $BM"Pilih No==> " pil;
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/nilkahis-cyber
+cd hackfb
+python2 brute.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil=17 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep1
+git clone https://github.com/Bocilx7/dark1.7
+cd dark1.7
+python2 darkvip.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/GUNAWAN18ID/cloning-yahoo.git
+cd cloning-yahoo
+python2 yahoo.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/Zxulme/ZXU1m3
+cd ZXU1m3
+pip2 install requests
+pip2 install mechanize
+python2 DarkFB.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/IqbalDev/coba
+cd coba
+python2 marshal.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/muhammadfathul/FBH
+cd FBH
+chmod +x *
+bash setup
+./FBH
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+pip2 install mechanize
+pip install requests
+pip2 install requests
+python2 SETAN.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+apt update
+apt upgrade
+apt insta git
+git clone https://github.com/AlatToll/BotFb
+cd BotFb
+python2 BotFb.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install git
+pkg install php
+git clone https://github.com/PangeranAlvins/ppguard
+cd ppguard
+php guard.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+python2 Token.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/GUNAWAN18ID/facebot
+cd facebot
+python2 facebot.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+git clone https://github.com/KANG-NEWBIE/s-mbf.git
+cd s-mbf
+python2 embf.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+sleep 1
+git clone https://github.com/zevtyardt/fb-maker
+cd fb-maker
+python2 fb-maker.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slnat "W A I T" | lolcat
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+cd hack-facebook-teman
+sh requests.sh
+python2 hack-fb.py
+fi
+
+if [ $pil = 99 ]
+then
+clear
+figlet -f slant "W A I T" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install vim
+pkg install cowsay
+pkg install figlet
+pkg install lolcat
+gem install lolcat
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant "D O N E"|lolcat
+sleep 1
+sh bengkelFacebook.sh
+fi
+
+if [ $pil = 00 ]
+then
+clear
+figlet -f slant "E.X.I.T" | lolcat
+figlet -f slant "THANKS" | lolcat
+sleep 2
+echo $Bt"Jngn di recode yh hargai Author;)"
+sleep 2
+echo $Bm"KALO MAU HACK FB TARGET BELI DI SAYA TOOLSNYA"
+sleep 2.5
+echo $C"GUNAKAN DENGAN BIJAK,HUBUNGI WA 085747407420;)"
+sleep 2
+echo $HT"SELAMAT TINGGALv:)"
+echo $ by Agung Bochilx7
+sleep 2.6
+exit
+fi
+
